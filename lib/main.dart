705b5c729befcb05ece2fc139f7c7cf9e4b993eb
kako351 <h3.birth@gmail.com>
2024-12-15 16:55:24 +0900

    device_previewを設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {

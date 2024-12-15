2442fcdfd9e9f1b80e9eabec0db7d0c4129db944
kako351 <h3.birth@gmail.com>
2024-12-15 17:04:43 +0900

    testコード修正

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_brew/main.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      DevicePreview(
+        enabled: true,
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

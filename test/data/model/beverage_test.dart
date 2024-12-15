636cfe3b7ee622a50374eeb46a89546d71931a82
kako351 <h3.birth@gmail.com>
2024-12-15 17:38:11 +0900

    expect追加

--- test/data/model/beverage_test.dart
+++ test/data/model/beverage_test.dart
@@ -16,5 +16,8 @@ void main() {
     final beverage = Beverage.fromJson(mockJson);
     expect(beverage.id, 1);
     expect(beverage.title, 'Black Coffee');
+    expect(beverage.description, 'Svart kaffe är så enkelt som det kan bli med malda kaffebönor dränkta i hett vatten, serverat varmt. Och om du vill låta fancy kan du kalla svart kaffe med sitt rätta namn: café noir.');
+    expect(beverage.ingredients, ['Coffee']);
+    expect(beverage.image, 'https://images.unsplash.com/photo-1494314671902-399b18174975?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
   });
 }

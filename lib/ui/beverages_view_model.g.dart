ffcf28c9f0ee740f0fb502b98b00a262ed98d360
kako351 <h3.birth@gmail.com>
2024-12-15 23:38:24 +0900

    view model 追加

---
+++ lib/ui/beverages_view_model.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'beverages_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$beveragesViewModelHash() =>
+    r'85fd6e12f20821c94a3c8243fcce36066d801d9c';
+
+/// See also [BeveragesViewModel].
+@ProviderFor(BeveragesViewModel)
+final beveragesViewModelProvider = AutoDisposeAsyncNotifierProvider<
+    BeveragesViewModel, BeveragesViewState>.internal(
+  BeveragesViewModel.new,
+  name: r'beveragesViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$beveragesViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BeveragesViewModel = AutoDisposeAsyncNotifier<BeveragesViewState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
---
+++ lib/ui/viewstate/beverages_view_state.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter_brew/data/model/beverage.dart';
+
+abstract interface class BeveragesViewState {}
+
+class LoadingBeveragesViewState implements BeveragesViewState {}
+
+class ErrorBeveragesViewState implements BeveragesViewState {}
+
+class SuccessBeveragesViewState implements BeveragesViewState {
+  final List<Beverage> beverages;
+
+  SuccessBeveragesViewState(this.beverages);
+}

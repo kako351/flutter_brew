9a194d639d5840bd23773310932bcb1f66696447
kako351 <h3.birth@gmail.com>
2024-12-15 21:30:48 +0900

    api service追加

---
+++ lib/data/infra/api_service.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_brew/data/model/beverage.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: 'https://api.sampleapis.com')
+abstract class ApiService {
+  factory ApiService(Dio dio, {String? baseUrl}) = _ApiService;
+
+  @GET('/coffee/hot')
+  Future<List<Beverage>> getBeverages();
+}

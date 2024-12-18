8ffdfc79193fd1685e4d45e41cc101a84b6862f2
kako351 <h3.birth@gmail.com>
2024-12-18 23:06:34 +0900

    MyAppの設定を追加

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: 'Flutter Brew',
+      debugShowCheckedModeBanner: !kReleaseMode,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
       routerConfig: router,
     );
   }

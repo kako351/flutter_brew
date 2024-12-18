3adaa12f384bec3a8212c3844fcb80fa30ec8788
kako351 <h3.birth@gmail.com>
2024-12-18 22:23:35 +0900

    不要なimport削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter_brew/ui/beverage_list.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_brew/ui/route/router.dart';
 

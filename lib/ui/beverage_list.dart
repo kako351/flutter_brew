967b02eb68995ca46a82d27c209dee06c46e2cd2
kako351 <h3.birth@gmail.com>
2024-12-18 22:59:07 +0900

    画面遷移

--- lib/ui/beverage_list.dart
+++ lib/ui/beverage_list.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_brew/ui/beverages_view_model.dart';
 import 'package:flutter_brew/ui/viewstate/beverages_view_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 
 class BeverageListPage extends ConsumerStatefulWidget {
@@ -47,8 +48,11 @@ class BeverageList extends ConsumerWidget {
           return ListView.builder(
             itemCount: beverages.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text('${beverages[index]}'),
+              return GestureDetector(
+                  onTap: () {
+                    context.push('/beverage/${beverages[index].id}');
+                  },
+                  child: ListTile(title: Text('${beverages[index]}')),
               );
             },
           );
--- lib/ui/route/router.dart
+++ lib/ui/route/router.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:flutter_brew/ui/beverage_detail.dart';
 import 'package:flutter_brew/ui/beverage_list.dart';
 import 'package:go_router/go_router.dart';
 
@@ -5,7 +8,13 @@ GoRouter router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
+      name: 'beverage_list',
       builder: (context, state) => BeverageListPage(),
     ),
+    GoRoute(
+      path: '/beverage/:id',
+      name: 'beverage_detail',
+      builder: (context, state) => BeverageDetailPage(id: int.parse(state.pathParameters['id'] as String)),
+    )
   ],
 );

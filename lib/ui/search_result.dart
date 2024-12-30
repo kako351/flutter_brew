5ca1f6b3adb31ee3335558e8193ae9ba7c3335e8
kako351 <h3.birth@gmail.com>
2024-12-30 18:11:55 +0900

    レイアウト修正

--- lib/ui/search_result.dart
+++ lib/ui/search_result.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_brew/data/model/beverage.dart';
+import 'package:flutter_brew/ui/designsystem/color.dart';
 import 'package:flutter_brew/ui/search_result_view_model.dart';
 import 'package:flutter_brew/ui/viewstate/search_result_view_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -47,15 +49,79 @@ class SearchResult extends ConsumerWidget {
             return ListView.builder(
               itemCount: viewState.beverages.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(viewState.beverages[index].title),
-                  subtitle: Text(viewState.beverages[index].description),
+                return SearchResultItem(
+                  beverage: viewState.beverages[index],
+                  onTap: () {
+
+                  }
                 );
               },
             );
           }
         },
       ),
+    );
+  }
+}
+
+class SearchResultItem extends StatelessWidget {
+  const SearchResultItem({required this.beverage, required this.onTap, super.key});
+
+  final Beverage beverage;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Hero(
+                tag: beverage.imageHeroTag,
+                child: ClipOval(
+                  child: Image.network(
+                    beverage.image,
+                    key: Key(beverage.title),
+                    width: 100.0,
+                    height: 100.0,
+                    cacheWidth: 537,
+                    cacheHeight: 807,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width - 132.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Hero(
+                        tag: beverage.titleHeroTag,
+                        child: Text(beverage.title, style: Theme.of(context).textTheme.labelLarge
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 8.0),
+                      ),
+                      Text(
+                        beverage.ingredients.join(', '),
+                        style: Theme.of(context).textTheme.labelSmall?.merge(TextStyle(color: BrewColor.lightGrey)),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
     );
   }
 }
